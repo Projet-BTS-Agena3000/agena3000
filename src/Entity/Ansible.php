eab29c5e40049b6c17405885d11defa035ee1a7d
Alexandre Richard <86735269+AureusAxis@users.noreply.github.com>
2022-03-24 15:36:14 +0100

    test password

--- src/Entity/Ansible.php
+++ src/Entity/Ansible.php
@@ -13,7 +13,10 @@ class Ansible
 
     public function installer($nomService)
     {
-        shell_exec('ansible-playbook ../../playbooks/install/' . $nomService . '.yml --ask-become-pass');
-        
+        $password = 'BtsSn2022';
+        $output = shell_exec('ansible-playbook ../../playbooks/install/' . $nomService . '.yml --ask-become-pass');
+        if($output == ''){
+            shell_exec($password);
+        }
     }
 }

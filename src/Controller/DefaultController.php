914a5b33c5b1b17518cc76f8ac0ddcc3dd882b59
Alexandre Richard <86735269+AureusAxis@users.noreply.github.com>
2022-04-05 10:56:44 +0200

    ajout min intType

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -62,7 +62,8 @@ class DefaultController extends AbstractController{
             -> add('Vlan', IntegerType::class, [
                 'label' => 'N° VLAN',
                 'attr' => ['class' => 'vlan'],
-                'label_attr' => ['class' => 'labelText']
+                'label_attr' => ['class' => 'labelText'],
+                'min' => 0
             ])
             ->add("NbCoeurs", RangeType::class,[
                 "label" => "Nombre de Coeurs",
@@ -94,7 +95,8 @@ class DefaultController extends AbstractController{
             ->add("NbInterfaces", IntegerType::class,[
                 'attr' => ['class' => 'nbInterfaces'],
                 'label' => "Nombres d'Interfaces Réseaux",
-                'label_attr' => ['class' => 'labelText']
+                'label_attr' => ['class' => 'labelText'],
+                'min' => 0
             ])
             ->add('submit', SubmitType::class, [
                 'attr' => ['class' => 'bouton']
